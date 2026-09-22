2d580e022270cd92073f08501e49a72d57667e31
DiegozRM <diegoromomuxoz@gmail.com>
2026-09-22 07:26:00 -0600

    feat(parcial02) - add mediator and other changes

---
+++ src/EventsHub.Application/Events/Queries/GetEventDetails.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using EventsHub.Domain;
+using EventsHub.Persistence;
+using MediatR;
+
+namespace EventsHub.Application.Events.Queries
+{
+    public class GetEventDetails
+    {
+        public class Query: IRequest<Event>
+        {
+            public required string Id { get; set; }
+        }
+
+        public class Handler(AppDbContext context) : IRequestHandler<Query, Event>
+        {
+            public async Task<Event> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var result = await context.Events.FindAsync(request.Id, cancellationToken) 
+                ?? throw new Exception("Activity not found");
+
+                return result;
+            }
+        }
+    }
+}
